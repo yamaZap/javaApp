b1b6c15a9c3ffddae9ee387ea9b58fa75ea6a533
He-WenBin <uk936504@yahoo.com.tw>
2014-06-30 16:56:44 +0800

    復原了

--- src/javaApp/Main.java
+++ src/javaApp/Main.java
@@ -5,8 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int y = 123;
-		System.out.println(y);
+		int x = 123;
+		System.out.println(x);
 	}
 
 }
