efee9c6df5e5df6e86d05946b6b2a198c461fe19
He-WenBin <uk936504@yahoo.com.tw>
2014-06-30 16:46:28 +0800

    變成y

--- src/javaApp/Main.java
+++ src/javaApp/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x = 123;
-		System.out.println(x);
+		int y = 123;
+		System.out.println(y);
 	}
 
 }
